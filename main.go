03c1a08b4571b4f2ecd560b62ea1bcb6ed7ccbfb
Montreuil <montreuiltommy@gmail.com>
2019-08-11 16:44:47 -0400

    Added a logger and setup port to get environment variable

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"log"
 	"math/rand"
 	"net/http"
+	"os"
 )
 
 type Data struct {
@@ -20,24 +21,32 @@ type Tuple struct {
 }
 
 func main() {
-	const port = ":443"
+	port := getPort()
 	router := mux.NewRouter()
 	originsOk := handlers.AllowedOrigins([]string{"http://localhost:3000", "https://lapx.github.io"})
 	router.HandleFunc("/", getRoot).Methods("GET")
 	router.HandleFunc("/data", getData).Methods("GET")
 	router.Use(mux.CORSMethodMiddleware(router))
-	fmt.Println("[INFO] Api listening on port " + port)
-	http.ListenAndServe(port, handlers.CORS(originsOk)(router))
+	log.Println("[INFO] Api listening on port " + port)
+	log.Fatal(http.ListenAndServe(port, handlers.CORS(originsOk)(router)))
+}
+
+func getPort() string {
+	port := ":" + os.Getenv("PORT")
+	if port == ":" {
+		port = ":8080"
+	}
+	return port
 }
 
 func getRoot(response http.ResponseWriter, request *http.Request) {
-	fmt.Println("[INFO] / got called")
+	log.Println("[INFO] / got called")
 	response.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(response).Encode("The api works")
 }
 
 func getData(response http.ResponseWriter, request *http.Request) {
-	fmt.Println("[INFO] /data got called")
+	log.Println("[INFO] /data got called")
 	response.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(response).Encode(generateRandomDataList(rand.Intn(6) + 1))
 }
